5627f8a6ea8fadfa2498f0cb9444d06524293da1
Ryno Andryano <rynoandryano@gmail.com>
2023-08-03 10:55:58 +0700

    Set up Spring MVC configuration

---
+++ src/main/java/com/prosigmaka/WebConfig.java
@@ -0,0 +1,19 @@
+package com.prosigmaka;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.prosigmaka"})
+public class WebConfig implements WebMvcConfigurer {
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setPrefix("/WEB-INF/view/");
+        vr.setSuffix(".jsp");
+        return vr;
+    }
+}
